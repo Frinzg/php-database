6fecb149442b0871fd62b4f7f65a6d2e8ae77fd7
Danang Triyatno <danangtriyatno180@gmail.com>
2026-06-05 11:07:05 +0700

    first commit

--- index.php
+++ index.php
@@ -13,140 +13,191 @@ $data1 = $mhs->getData();
 $data2 = $mtkl->getData();
 $data3 = $dosen->getData();
 $data4 = $perkuliahan->getData();
+$page = $_GET['page'] ?? 'home';
 ?>
 
-<h2 class="text-center mt-3">Tabel Mahasiswa</h2>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
 
-<div class="container mt-3">
+<body>
 
-<table class="table table-bordered table-striped">
-    <thead>
-        <tr>
-            <th>NIM</th>
-            <th>Nama Mahasiswa</th>
-            <th>Angkatan</th>
-            <th>Alamat</th>
-            <th>Email</th>  
-        </tr>
-    </thead>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Sistem Akademik</a>
 
-    <tbody>
-    <?php foreach ($data1 as $row) { ?>
-        <tr>
-            <td><?= $row['nim']; ?></td>
-            <td><?= $row['nama_mhs']; ?></td>
-            <td><?= $row['angkatan']; ?></td>
-            <td><?= $row['alamat']; ?></td>
-            <td><?= $row['email']; ?></td>
-        </tr>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav me-auto">
+
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?page=mahasiswa">Mahasiswa</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?page=dosen">Dosen</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?page=matkul">Matkul</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?page=perkuliahan">Perkuliahan</a>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="container mt-4">
+
+<?php if ($page == 'mahasiswa') { ?>
+
+    <h2 class="text-center mt-3">Tabel Mahasiswa</h2>
+
+        <div class="container mt-3">
+
+        <table class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>NIM</th>
+                    <th>Nama Mahasiswa</th>
+                    <th>Angkatan</th>
+                    <th>Alamat</th>
+                    <th>Email</th>  
+                </tr>
+            </thead>
+
+            <tbody>
+            <?php foreach ($data1 as $row) { ?>
+                <tr>
+                    <td><?= $row['nim']; ?></td>
+                    <td><?= $row['nama_mhs']; ?></td>
+                    <td><?= $row['angkatan']; ?></td>
+                    <td><?= $row['alamat']; ?></td>
+                    <td><?= $row['email']; ?></td>
+                </tr>
+            <?php } ?>
+            </tbody>
+
+        </table>
+
+<?php } elseif ($page == 'dosen') { ?>
+
+   <h2 class="text-center mt-3">Tabel Dosen</h2>
+    </div>
+
+        <div class="container mt-3">
+
+        <table class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>Kode Dosen</th>
+                    <th>Nama Dosen</th>
+                    <th>Email</th>
+                    <th>Jabatan Fungsional</th>
+                    <th>No. Telepon</th>
+                    <th>Program Studi</th>
+                </tr>
+            </thead>
+
+            <tbody>
+            <?php foreach ($data3 as $row) { ?>
+                <tr>
+                    <td><?= $row['nidn']; ?></td>
+                    <td><?= $row['nama_dosen']; ?></td>
+                    <td><?= $row['email']; ?></td>
+                    <td><?= $row['jabatan_fungsional']; ?></td>
+                    <td><?= $row['no_telp']; ?></td>
+                    <td><?= $row['prodi']; ?></td>
+                </tr>
+            <?php } ?>
+            </tbody>
+
+        </table>
+
+    </div>
+    
+
+
+<?php } elseif ($page == 'matkul') { ?>
+
+    <h2 class="text-center mt-3">Tabel Matakuliah</h2>
+        </div>
+
+        <div class="container mt-3">
+
+        <table class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>Kode Matakuliah</th>
+                    <th>Nama Matakuliah</th>
+                    <th>SKS</th>
+                    <th>Semester</th>
+                </tr>
+            </thead>
+
+            <tbody>
+            <?php foreach ($data2 as $row) { ?>
+                <tr>
+                    <td><?= $row['kode_matkul']; ?></td>
+                    <td><?= $row['nama_matkul']; ?></td>
+                    <td><?= $row['sks']; ?></td>
+                    <td><?= $row['semester']; ?></td>
+                </tr>
+            <?php } ?>
+            </tbody>
+
+        </table>
+
+        </div>
+
+<?php } else { ?>
+
+    <h2 class="text-center mt-3">Tabel Perkuliahan</h2>
+    </div>
+
+    <div class="container mt-3">
+
+    <table class="table table-bordered table-striped">
+        <thead>
+            <tr>
+                <th>Kode Perkuliahan</th>
+                <th>Kode Dosen</th>
+                <th>Kode Matakuliah</th>
+                <th>Semester</th>
+                <th>Nilai</th>
+                <th>Tahun Akademik</th>
+            </tr>
+        </thead>
+
+        <tbody>
+        <?php foreach ($data4 as $row) { ?>
+            <tr>
+                <td><?= $row['id_perkuliahan']; ?></td>
+                <td><?= $row['nim']; ?></td>
+                <td><?= $row['kode_matkul']; ?></td>
+                <td><?= $row['nidn']; ?></td>
+                <td><?= $row['semester']; ?></td>
+                <td><?= $row['nilai']; ?></td>
+                <td><?= $row['tahun_akademik']; ?></td>
+            </tr>
+        <?php } ?>
+        </tbody>
+
+    </table>
+
+    </div>
+
     <?php } ?>
-    </tbody>
 
-</table>
-
-<br>
-<br>
-<h2 class="text-center mt-3">Tabel Matakuliah</h2>
-</div>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
-<div class="container mt-3">
-
-<table class="table table-bordered table-striped">
-    <thead>
-        <tr>
-            <th>Kode Matakuliah</th>
-            <th>Nama Matakuliah</th>
-            <th>SKS</th>
-            <th>Semester</th>
-        </tr>
-    </thead>
-
-    <tbody>
-    <?php foreach ($data2 as $row) { ?>
-        <tr>
-            <td><?= $row['kode_matkul']; ?></td>
-            <td><?= $row['nama_matkul']; ?></td>
-            <td><?= $row['sks']; ?></td>
-            <td><?= $row['semester']; ?></td>
-        </tr>
-    <?php } ?>
-    </tbody>
-
-</table>
-
-</div>
-<br>
-<br>
-<h2 class="text-center mt-3">Tabel Dosen</h2>
-</div>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
-<div class="container mt-3">
-
-<table class="table table-bordered table-striped">
-    <thead>
-        <tr>
-            <th>Kode Dosen</th>
-            <th>Nama Dosen</th>
-            <th>Email</th>
-            <th>Jabatan Fungsional</th>
-            <th>No. Telepon</th>
-            <th>Program Studi</th>
-        </tr>
-    </thead>
-
-    <tbody>
-    <?php foreach ($data3 as $row) { ?>
-        <tr>
-            <td><?= $row['nidn']; ?></td>
-            <td><?= $row['nama_dosen']; ?></td>
-            <td><?= $row['email']; ?></td>
-            <td><?= $row['jabatan_fungsional']; ?></td>
-            <td><?= $row['no_telp']; ?></td>
-            <td><?= $row['prodi']; ?></td>
-        </tr>
-    <?php } ?>
-    </tbody>
-
-</table>
-
-</div>
-<br>
-<br>
-<h2 class="text-center mt-3">Tabel Perkuliahan</h2>
-</div>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
-<div class="container mt-3">
-
-<table class="table table-bordered table-striped">
-    <thead>
-        <tr>
-            <th>Kode Perkuliahan</th>
-            <th>Kode Dosen</th>
-            <th>Kode Matakuliah</th>
-            <th>Semester</th>
-            <th>Nilai</th>
-            <th>Tahun Akademik</th>
-        </tr>
-    </thead>
-
-    <tbody>
-    <?php foreach ($data4 as $row) { ?>
-        <tr>
-            <td><?= $row['id_perkuliahan']; ?></td>
-            <td><?= $row['nim']; ?></td>
-            <td><?= $row['kode_matkul']; ?></td>
-            <td><?= $row['nidn']; ?></td>
-            <td><?= $row['semester']; ?></td>
-            <td><?= $row['nilai']; ?></td>
-            <td><?= $row['tahun_akademik']; ?></td>
-        </tr>
-    <?php } ?>
-    </tbody>
-
-</table>
-
-</div>
+        
+    </body>
+    </html>
